222c7f3b461fe3146cc536ac008ce98b150c9341
eli-stewart <eliwstewart10@gmail.com>
2017-04-18 22:08:18 -0700

    deleted commas

--- checkers.js
+++ checkers.js
@@ -1,101 +1,101 @@
 var pceR1 = {
     location: [2, 8],
     kingstate: false,
-, };
+};
 var pceR2 = {
     location: [4, 8],
     kingstate: false,
-, };
+ };
 var pceR3 = {
     location: [6, 8],
     kingstate: false,
-, };
+ };
 var pceR4 = {
     location: [8, 8],
     kingstate: false,
-, };
+ };
 var pceR5 = {
     location: [1, 7],
     kingstate: false,
-, };
+ };
 var pceR6 = {
     location: [3, 7],
     kingstate: false,
-, };
+ };
 var pceR7 = {
     location: [5, 7],
     kingstate: false,
-, };
+ };
 var pceR8 = {
     location: [7, 7],
     kingstate: false,
-, };
+ };
 var pceR9 = {
     location: [2, 6],
     kingstate: false,
-, };
+ };
 var pceR10 = {
     location: [4, 6],
     kingstate: false,
-, };
+ };
 var pceR11 = {
     location: [6, 6],
     kingstate: false,
-, };
+ };
 var pceR12 = {
     location: [8, 6],
     kingstate: false,
-, };
+ };
 
 
 var pceW1 = {
     location: [1, 1],
     kingstate: false,
-, };
+ };
 var pceW2 = {
     location: [3, 1],
     kingstate: false,
-, };
+ };
 var pceW3 = {
     location: [5, 1],
     kingstate: false,
-, };
+ };
 var pceW4 = {
     location: [7, 1],
     kingstate: false,
-, };
+ };
 var pceW5 = {
     location: [2, 2],
     kingstate: false,
-, };
+ };
 var pceW6 = {
     location: [4, 2],
     kingstate: false,
-, };
+ };
 var pceW7 = {
     location: [6, 2],
     kingstate: false,
-, };
+ };
 var pceW8 = {
     location: [8, 2],
     kingstate: false,
-, };
+ };
 var pceW9 = {
     location: [1, 3],
     kingstate: false,
-, };
+ };
 var pceW10 = {
     location: [4, 3],
     kingstate: false,
-, };
+ };
 var pceW11 = {
     location: [6, 3],
     kingstate: false,
-, };
+ };
 var pceW12 = {
     location: [8, 3],
     kingstate: false,
-, };
+ };
 
 var selected = 0;
 var pceArrayRed = [pceR1, pceR2, pceR3, pceR4, pceR5, pceR6, pceR7, pceR8, pceR9, pceR10, pceR11, pceR12];
